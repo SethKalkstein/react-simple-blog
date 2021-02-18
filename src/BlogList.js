d2310d9455dab62d2e3e609a7ad757d2671dabf7
SethKalkstein <sethkal7@yahoo.com>
2021-02-18 13:17:37 -0500

    sending functions as props to delete an object from an array

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,11 @@
 // const BlogList = (props) => {
 //     const blogs = props.blogs;
 //     const title = props.title;
-const BlogList = ({ blogs, title }) => {
-//destructuring {} inside the function argument accomplishes the same goal as taking props for the argument and declaring the variables below as done in the comment above.
+const BlogList = ({ blogs, title, handleDelete }) => {
+//destructuring {} inside the function parameters accomplishes the same goal as taking props for the argument and declaring the variables below as done in the comment above.
+
+    // const handleDelete = (id) => setBlog(blogs.filter(blog => blog.id != id)); this could be accomplished by sending the setBlog function as a prop to this conponent instead of sending this function to this prop
+
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
@@ -11,6 +14,7 @@ const BlogList = ({ blogs, title }) => {
                    <div className="blog-preview" key={blog.id}>
                        <h2>{ blog.title }</h2>
                        <p>Written by: {blog.author} </p>
+                       <button onClick={ () => handleDelete(blog.id) }>Delete</button>
                     </div> 
                 ))
             }
