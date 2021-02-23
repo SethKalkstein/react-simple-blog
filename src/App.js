46200accddd6f4d7d98b0943918096e3b16cf3c5
SethKalkstein <sethkal7@yahoo.com>
2021-02-23 13:52:30 -0500

    added routing package and basic root route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from './NavBar';
 import Home from "./Home";
 import BlogComponent from "./BlogComponent";
 import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   const title = "Welcome to my newest blog";
@@ -10,17 +11,23 @@ function App() {
   useEffect( () => console.log("use effect has fired from the main app component") );
 
   return (
-    <div className="App">
-      <NavBar />
-      <div className="content">
-        <Home />
-        <p>liked {likes} times</p>
-        <p>{10}</p>
-        <p>{likes > 40 ? "lots of likes" : "Not enough likes"}</p>
-        <br/>
-        <BlogComponent />
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+              <p>liked {likes} times</p>
+              <p>{10}</p>
+              <p>{likes > 40 ? "lots of likes" : "Not enough likes"}</p>
+              <br/>
+              <BlogComponent />
+            </Route>  
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
