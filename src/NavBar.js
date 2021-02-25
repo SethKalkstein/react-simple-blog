c18d29409ccd42fb72ac9f514c75d66652fdf833
SethKalkstein <sethkal7@yahoo.com>
2021-02-25 18:36:01 -0500

    used Link tag instead of anchor tab for links within the domain.

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Sample Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
+{/*                 <a href="/">Home</a>
                 <a href="/create" style={{
                     //inline style written in object format with camel case keys
                     color: "white",
                     backgroundColor: "#f1356d",
                     borderRadius: "8px"
-                }}>New Blog</a>
+                }}>New Blog</a> */}
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
 
