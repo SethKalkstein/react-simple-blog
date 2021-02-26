3ffbcccc8598966e984d4e930383ff5ec9b7b5bf
SethKalkstein <sethkal7@yahoo.com>
2021-02-25 20:49:18 -0500

    added cleanup function to fetch with AbortController Object for when data is still loading during a route change.

--- src/BlogComponent.js
+++ src/BlogComponent.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
-import useFetch from "./usefetch";
+import useFetch from "./useFetch";
 
 const BlogComponent = () => {
     const { data: blogs, isLoading, error} = useFetch("http://localhost:8000/blogs");
--- src/NavBar.js
+++ src/NavBar.js
@@ -13,7 +13,12 @@ const NavBar = () => {
                     borderRadius: "8px"
                 }}>New Blog</a> */}
                 <Link to="/">Home</Link>
-                <Link to="/create">New Blog</Link>
+                <Link to="/create" style={{
+                    //inline style written in object format with camel case keys
+                    color: "white",
+                    backgroundColor: "#f1356d",
+                    borderRadius: "8px"
+                }}>New Blog</Link>
             </div>
         </nav>
 
